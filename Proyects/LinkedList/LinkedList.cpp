da282a659ecb0a14215cfe8b63c2b1df9eedace0
marcos <marcosbuydid@gmail.com>
2023-06-08 21:14:00 -0300

    add insertion and delete methods

--- Proyects/LinkedList/LinkedList.cpp
+++ Proyects/LinkedList/LinkedList.cpp
@@ -133,21 +133,96 @@ NodeList* searchNodeRecursively(NodeList *p, int number) {
 	searchNodeRecursively(p->next, number);
 }
 
+void insertNode(NodeList* &list, int number, int pos) {
+	if (list->next == NULL || pos == 0) {
+		NodeList* node = new NodeList();
+		node->data = number;
+		node->next = list;
+		list = node;
+	}
+	else {
+		if (pos > 0) {
+			NodeList* p = list;
+
+			for (int i = 1; i < pos && p->next != NULL; i++) {
+				p = p->next;
+			}
+			NodeList* node = new NodeList();
+			node->data = number;
+			node->next = p->next;
+			p->next = node;
+		}
+	}
+}
+
+void insertNodeInSortedList(NodeList* &list, int number) {
+	if (list->next == NULL || list->data >= number) {
+		NodeList* node = new NodeList();
+		node->data = number;
+		node->next = list;
+		list = node;
+	}
+	else {
+		insertNodeInSortedList(list->next, number);
+	}
+}
+
+void deleteNode(NodeList* &list, int number) {
+	if (list != NULL) {
+		if (list->data == number) {
+			NodeList* nodeToDelete = list;
+			list = list->next;
+			delete nodeToDelete;
+		}
+		else {
+			deleteNode(list->next, number);
+		}
+	}
+}
+
+void deleteNodeByPosition(NodeList* &list, int pos) {
+	if (list != NULL) {
+		if (pos == 0) {
+			NodeList* nodeToDelete = list;
+			list = list->next;
+			delete nodeToDelete;
+		}
+		else {
+			if (pos > 0) {
+				for (int i = 1; i <= pos && list->next != NULL; i++) {
+					list = list->next;
+				}
+				NodeList* nodeToDelete = list;
+				list = list->next;
+				delete nodeToDelete;
+			}
+		}
+	}
+}
+
 int main() {
 
-	int A[] = { 2,4,6,8,10,12,14,16 };
-	NodeList *p = createLinkedList(A, 8);
+	int A[] = { 2,1,6,3,30,52,14,26 };
+	NodeList *list = createLinkedList(A, 8);
 
-	//DisplayNodeListData(p); 
-	//RecursiveDisplayNodeListData(p);
-	//cout << NodeCounter(p);
-	//cout << RecursiveNodeCounter(p);
-	//cout << NodeDataSum(p);
-	//cout << RecursiveNodeDataSum(p);
-	//cout << maxNodeData(p);
-	//cout << recursiveMaxNodeData(p);
-	//cout << searchNode(p, 14);
-	cout << searchNodeRecursively(p, 14);
+	int B[] = { 1,3,5,8,9,10 };
+	NodeList *sortedList = createLinkedList(B, 6);
+
+	//DisplayNodeListData(list); 
+	//RecursiveDisplayNodeListData(list);
+	//cout << NodeCounter(list);
+	//cout << RecursiveNodeCounter(list);
+	//cout << NodeDataSum(list);
+	//cout << RecursiveNodeDataSum(list);
+	//cout << maxNodeData(list);
+	//cout << recursiveMaxNodeData(list);
+	//cout << searchNode(list, 14);
+	//cout << searchNodeRecursively(list, 14);
+	//insertNode(list, 20, 8);
+	//insertNodeInSortedList(sortedList, 4);
+	//deleteNode(list, 30);
+	deleteNodeByPosition(list, 1);
+	DisplayNodeListData(list);
 
 	return 0;
 }
