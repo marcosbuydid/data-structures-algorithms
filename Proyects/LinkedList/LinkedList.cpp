cf22357baa1508d4a62de6cbce25be83b177f486
marcosbuydid <marcosbuydid@gmail.com>
2023-09-27 01:40:35 -0300

    add some methods

--- Proyects/LinkedList/LinkedList.cpp
+++ Proyects/LinkedList/LinkedList.cpp
@@ -29,7 +29,7 @@ NodeList* createLinkedList(int A[], int n) {
 	return first;
 }
 
-void DisplayNodeListData(NodeList* l) {
+void displayNodeListData(NodeList* l) {
 
 	while (l->next != NULL) {
 		printf("%d \n", l->data);
@@ -37,15 +37,15 @@ void DisplayNodeListData(NodeList* l) {
 	}
 }
 
-void RecursiveDisplayNodeListData(NodeList* l) {
+void displayNodeListDataRec(NodeList* l) {
 
 	if (l->next != NULL) {
 		printf("%d \n", l->data);
-		RecursiveDisplayNodeListData(l->next);
+		displayNodeListDataRec(l->next);
 	}
 }
 
-int NodeCounter(NodeList* l) {
+int nodeCounter(NodeList* l) {
 
 	int quantity = 0;
 	if (l == NULL) {
@@ -60,17 +60,17 @@ int NodeCounter(NodeList* l) {
 	}
 }
 
-int RecursiveNodeCounter(NodeList* l) {
+int nodeCounterRec(NodeList* l) {
 
 	if (l->next == NULL) {
 		return 0;
 	}
 	else {
-		return 1+ RecursiveNodeCounter(l->next);
+		return 1+ nodeCounterRec(l->next);
 	}
 }
 
-int NodeDataSum(NodeList* l) {
+int nodeDataSum(NodeList* l) {
 
 	int sum = 0;
 	if (l == NULL) {
@@ -85,17 +85,17 @@ int NodeDataSum(NodeList* l) {
 	}
 }
 
-int RecursiveNodeDataSum(NodeList* l) {
+int nodeDataSumRec(NodeList* l) {
 
 	if (l->next == NULL) {
 		return 0;
 	}
 	else {
-		return l->data + RecursiveNodeDataSum(l->next);
+		return l->data + nodeDataSumRec(l->next);
 	}
 }
 
-int maxNodeData(NodeList* l) {
+int maxNode(NodeList* l) {
 
 	if (l == NULL) {
 		return 0;
@@ -113,13 +113,13 @@ int maxNodeData(NodeList* l) {
 	}
 }
 
-int recursiveMaxNodeData(NodeList* l) {
+int maxNodeRec(NodeList* l) {
 
 	if (l == NULL) {
 		return 0;
 	}
 	else {
-		int max = recursiveMaxNodeData(l->next);
+		int max = maxNodeRec(l->next);
 		if (max > l->data) {
 			return max;
 		}
@@ -144,7 +144,7 @@ NodeList* searchNode(NodeList* l, int number) {
 	}
 }
 
-NodeList* searchNodeRecursively(NodeList* l, int number) {
+NodeList* searchNodeRec(NodeList* l, int number) {
 
 	if (l->next == NULL) {
 		return NULL;
@@ -152,7 +152,7 @@ NodeList* searchNodeRecursively(NodeList* l, int number) {
 	if (l->data == number) {
 		return l;
 	}
-	searchNodeRecursively(l->next, number);
+	searchNodeRec(l->next, number);
 }
 
 void insertNodeAtFront(NodeList*& l, int number) {
@@ -194,6 +194,17 @@ void insertNodeInSortedList(NodeList*& list, int number) {
 	}
 }
 
+void insertNodeAtLast(NodeList*& list, int number) {
+	if (list == NULL) {
+		list = new NodeList();
+		list->data = number;
+		list->next = NULL;
+	}
+	else {
+		insertNodeAtLast(list->next, number);
+	}
+}
+
 void deleteNode(NodeList*& list, int number) {
 	if (list != NULL) {
 		if (list->data == number) {
@@ -204,15 +215,76 @@ void deleteNode(NodeList*& list, int number) {
 	}
 }
 
-void deleteNodeByPositionRecursively(NodeList*& list, int pos) {
+void deleteNodeByPositionRec(NodeList*& list, int pos) {
 	if (list != NULL) {
 		if (pos == 0) {
 			deleteNode(list, list->data);
 		}
 		else {
 			pos--;
-			deleteNodeByPositionRecursively(list->next, pos);
+			deleteNodeByPositionRec(list->next, pos);
 		}
+	}
+}
+
+NodeList* eraseRange(NodeList*& list, int startPos, int finalPos) {
+	if (list == NULL || startPos > finalPos || startPos < 1) {
+		return NULL;
+	}
+	else {
+		int posAux = 1;
+		NodeList* result = NULL;
+		while (list != NULL) {
+			if (posAux == startPos) {
+				while (startPos <= finalPos) {
+					deleteNode(list, list->data);
+					startPos++;
+				}
+			}
+			insertNodeAtLast(result, list->data);
+			list = list->next;
+			posAux++;
+		}
+		return result;
+	}
+}
+
+NodeList* eraseRangeRec(NodeList*& list, int startPos, int finalPos) {
+	if (list == NULL || startPos > finalPos || startPos < 1) {
+		return NULL;
+	}
+	if (startPos == 1 && finalPos >= 1) {
+		deleteNode(list, list->data);
+		eraseRangeRec(list, startPos, finalPos - 1);
+	}
+	else {
+		eraseRangeRec(list->next, startPos-1, finalPos - 1);
+	}
+	return list;
+}
+
+NodeList* arrayToList(int* a, unsigned int n) {
+	if (n < 1) {
+		return NULL;
+	}
+	else {
+		NodeList* result = NULL;
+		NodeList* temp = NULL;
+		NodeList* last = NULL;
+
+		result = new NodeList();
+		result->data = a[0];
+		result->next = NULL;
+		last = result;
+
+		for (int i = 1; i <= n; i++) {
+			temp = new NodeList();
+			temp->data = a[i];
+			temp->next = NULL;
+			last->next = temp;
+			last = temp;
+		}
+		return result;
 	}
 }
 
@@ -223,21 +295,30 @@ int main() {
 
 	int B[] = { 1,3,5,8,9,10 };
 	NodeList* sortedList = createLinkedList(B, 6);
+
+	int C[] = { 1,2,3,4,5,6,7,8 };
+	NodeList* listTwo = createLinkedList(C, 8);
+
+	int* p = A;
  
-	//RecursiveDisplayNodeListData(list);
-	//cout << NodeCounter(list);
-	//cout << RecursiveNodeCounter(list);
-	//cout << NodeDataSum(list);
-	//cout << RecursiveNodeDataSum(list);
-	//cout << maxNodeData(list);
-	//cout << recursiveMaxNodeData(list);
+	//displayNodeListDataRec(list);
+	//cout << nodeCounter(list);
+	//cout << nodeCounterRec(list);
+	//cout << nodeDataSum(list);
+	//cout << nodeDataSumRec(list);
+	//cout << maxNode(list);
+	//cout << maxNodeRec(list);
 	//cout << searchNode(list, 14);
-	//cout << searchNodeRecursively(list, 14);
+	//cout << searchNodeRec(list, 14);
 	//insertNodeAtPosition(list, 20, 8);
 	//insertNodeInSortedList(sortedList, 4);
-	deleteNodeByPositionRecursively(list, 6);
-	//DisplayNodeListData(sortedList);
-	DisplayNodeListData(list);
+	//deleteNodeByPositionRec(list, 6);
+	//displayNodeListData(sortedList);
+	//NodeList* auxList = eraseRange(listTwo, 2, 5);
+	//NodeList* auxList = eraseRangeRec(listTwo, 2, 5);
+	NodeList* auxList = arrayToList(p, 8);
+	displayNodeListData(auxList);
+	
 
 	return 0;
 }
--- Proyects/Trees/Trees.cpp
+++ Proyects/Trees/Trees.cpp
@@ -2,6 +2,11 @@
 #include <stdio.h>
 using namespace std;
 
+struct NodeList {
+    int data;
+    NodeList* next = NULL;
+};
+
 struct NodeAB {
     int data;
     NodeAB* left;
@@ -27,37 +32,92 @@ NodeAB* createBinaryTree(int nodeData[], int i, int n) {
     return root;
 }
 
-void preOrder(NodeAB* p) {
-    if (p) {
-        cout << p->data << ", " << flush;
-        preOrder(p->left);
-        preOrder(p->right);
+void preOrder(NodeAB* root) {
+    if (root != NULL) {
+        cout << root->data << ", " << flush;
+        preOrder(root->left);
+        preOrder(root->right);
     }
 }
 
-void inOrder(NodeAB* p) {
-    if (p) {
-        inOrder(p->left);
-        cout << p->data << ", " << flush;
-        inOrder(p->right);
+void inOrder(NodeAB* root) {
+    if (root != NULL) {
+        inOrder(root->left);
+        cout << root->data << ", " << flush;
+        inOrder(root->right);
     }
 }
 
-void postOrder(NodeAB* p) {
-    if (p) {
-        postOrder(p->left);
-        postOrder(p->right);
-        cout << p->data << ", " << flush;
+void postOrder(NodeAB* root) {
+    if (root != NULL) {
+        postOrder(root->left);
+        postOrder(root->right);
+        cout << root->data << ", " << flush;
     }
+}
+
+int minLevelK(NodeAB* root, int k) {
+    if (root == NULL || k < 1) {
+        return NULL;
+    }
+    
+    if (k == 1) {
+        return root->data;
+    }
+    else {
+       int minIzq = minLevelK(root->left, k - 1);
+       int minDer = minLevelK(root->right, k - 1);
+
+       if (minIzq < minDer) {
+           return minIzq;
+       }
+       else {
+           return minDer;
+       }
+    }
+}
+
+int maxNode(NodeAB* root) {
+    if (root == NULL) {
+        return 0;
+    }
+    else {
+        int maximum = max(maxNode(root->left), maxNode(root->right));
+        if (maximum > root->data) {
+            return maximum;
+        }
+        else {
+            return root->data;
+        }
+    }
+}
+
+void printRange(NodeAB* t, int min, int max) {
+	if (t == NULL) {
+		return;
+	}
+	else {
+        printRange(t->right, min, max);
+        if (t->data >= min && t->data <= max) {
+            cout << t->data << ", " << flush;
+        }
+        printRange(t->left, min, max);
+	}
 }
 
 
 int main() {
 
-    int data[] = { 1, 2, 3, 4, 5, 6, 6, 6, 6 };
+    //int data[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    int data[] = { 7, 4, 11, 2, 5, 8, 3, 10 };
     int n = sizeof(data) / sizeof(data[0]);
     NodeAB* root = createBinaryTree(data, 0, n);
-    preOrder(root);
+    //preOrder(root);
+    //inOrder(root);
+    //postOrder(root);
+    //cout << minLevelK(root, 2);
+    //cout << maxNode(root);
+    printRange(root, 3, 9);
 
     return 0;
 }
