f6f3af132445f691a3744aa0d4281ab67759289a
marcosbuydid <marcosbuydid@gmail.com>
2023-10-30 20:52:41 -0300

    create priority int queue and add some methods

--- Proyects/Queue/Queue.cpp
+++ Proyects/Queue/Queue.cpp
@@ -111,15 +111,126 @@ void display(IntQueue* q) {
 	}
 }
 
+/*
+* PriorityQueueList using two dinamic arrays and
+* a variable elementQuantity.
+* Implementation is bounded.
+*/
+
+struct PriorityIntQueue {
+	int* elements;
+	int* priority;
+	unsigned int bound;
+	unsigned int elementQuantity;
+};
+
+PriorityIntQueue* createPriorityIntQueue(int bound) {
+	PriorityIntQueue* pq = new PriorityIntQueue();
+	pq->elements = new int[bound];
+	pq->priority = new int[bound];
+	pq->elementQuantity = 0;
+	pq->bound = bound;
+	return pq;
+}
+
+bool isEmpty(PriorityIntQueue* pq) {
+	return pq->elementQuantity == 0;
+}
+
+bool isFull(PriorityIntQueue* pq) {
+	return pq->elementQuantity == pq->bound;
+}
+
+unsigned int numberOfElements(PriorityIntQueue* pq) {
+	return pq->elementQuantity;
+}
+
+void enqueue(PriorityIntQueue*& pq, int e, int p) {
+	if (!isFull(pq)) {
+		/*if we want the first elements with minor priority*/
+		/*int index = numberOfElements(pq) - 1;
+		while (index > -1 && p < pq->priority[index]) {
+			pq->elements[index + 1] = pq->elements[index];
+			pq->priority[index + 1] = pq->priority[index];
+			index--;
+		}
+		pq->elements[index + 1] = e;
+		pq->priority[index + 1] = p;
+		pq->elementQuantity++;*/
+
+		int index = numberOfElements(pq);
+
+		//we shift the elements that have a priority
+		//in the index bigger or equal to the one to enqueue.
+		//queue elements are ordered from bigger priority to minor.
+		while (index > -1 && p > pq->priority[index]) {
+			pq->elements[index] = pq->elements[index - 1];
+			pq->priority[index] = pq->priority[index - 1];
+			index--;
+		}
+		pq->elements[index + 1] = e;
+		pq->priority[index + 1] = p;
+		pq->elementQuantity++;
+	}
+}
+
+void dequeue(PriorityIntQueue*& pq) {
+	if (!isEmpty(pq)) {
+		for (int i = 1; i < pq->elementQuantity; i++) {
+			pq->elements[i-1] = pq->elements[i];
+			pq->priority[i - 1] = pq->priority[i];
+		}
+		pq->elementQuantity--;
+	}
+}
+
+int beginning(PriorityIntQueue* pq) {
+	if (!isEmpty(pq)) {
+		return pq->elements[0];
+	}
+}
+
+int priorityBeginning(PriorityIntQueue* pq) {
+	if (!isEmpty(pq)) {
+		return pq->priority[0];
+	}
+}
+
+PriorityIntQueue* clone(PriorityIntQueue* pq) {
+	PriorityIntQueue* clonedQueue = createPriorityIntQueue(pq->bound);
+	
+	for (int i = 0; i < pq->elementQuantity; i++) {
+		clonedQueue->elements[i] = pq->elements[i];
+		clonedQueue->priority[i] = pq->priority[i];
+	}
+	clonedQueue->elementQuantity = pq->elementQuantity;
+	return clonedQueue;
+}
+
+void destroy(PriorityIntQueue* pq) {
+	delete[] pq->elements;
+	delete[] pq->priority;
+	delete pq;
+}
+
+void display(PriorityIntQueue* pq) {
+	if (!isEmpty(pq)) {
+		for (int i = 0; i < pq->elementQuantity; i++) {
+			cout << pq->elements[i] << "|" << pq->priority[i] << " ";
+			cout << endl;
+		}
+	}
+}
+
 int main() {
 
-	IntQueue* iq = createIntQueue();
+	//IntQueue* iq = createIntQueue();
 
-	for (int i = 0; i < 6; i++) {
+	/*for (int i = 0; i < 6; i++) {
 		enqueue(iq, i);
-	}
+	}*/
 
-	dequeue(iq);
+	//dequeue(iq);
 
 	//cout << first(iq);
 
@@ -129,7 +240,33 @@ int main() {
 
 	//destroy(iq);
 
-	display(iq);
+	//display(iq);
+
+	PriorityIntQueue* pq = createPriorityIntQueue(10);
+
+	enqueue(pq, 1, 4);
+	enqueue(pq, 10, 3);
+	enqueue(pq, 25, 13);
+	enqueue(pq, 6, 4);
+	enqueue(pq, 9, 14);
+	enqueue(pq, 12, 6);
+	enqueue(pq, 8, 2);
+	enqueue(pq, 18, 1);
+	enqueue(pq, 31, 5);
+	enqueue(pq, -31, 5);
+
+
+	//dequeue(pq);
+
+	//cout << beginning(pq);
+
+	//cout << priorityBeginning(pq);
+
+	//PriorityIntQueue* clonedPQ = clone(pq);
+	
+	//destroy(pq);
+
+	display(pq);
 
 	return 0;
 }
