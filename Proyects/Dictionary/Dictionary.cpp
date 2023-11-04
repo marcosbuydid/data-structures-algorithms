0de5b909aab7a16f9be8415be201f93e2927884f
marcosbuydid <marcosbuydid@gmail.com>
2023-11-04 12:54:46 -0300

    create dictionary with some methods

---
+++ Proyects/Dictionary/Dictionary.cpp
@@ -0,0 +1,209 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+* IntDictionary using a pointer to a pointer of a LinkedList
+* and two variables, capacity and elementQuantity.
+* Add, erase and belongs operations are made in O(1) on average.
+* Open hashing is used with module operation suited as hash function.
+* Implementation is unbounded.
+*/
+
+struct NodeList {
+	int data;
+	NodeList* next;
+};
+
+struct IntDictionary {
+	NodeList** table;
+	unsigned int capacity;
+	unsigned int elementQuantity;
+};
+
+/*Auxiliary methods */
+
+int modHash(int element, int capacity) {
+	//capacidad must be prime
+	int position = element % capacity;
+	if (position < 0) {
+		position = position * -1;
+	}
+	return position;
+}
+
+void addElement(NodeList*& list, int e) {
+	NodeList* node = new NodeList();
+	node->data = e;
+	node->next = list;
+	list = node;
+}
+
+NodeList* cloneList(NodeList* list) {
+	if (list == NULL) {
+		return NULL;
+	}
+	else {
+		NodeList* clonedList = new NodeList();
+		clonedList->data = list->data;
+		clonedList->next = cloneList(list->next);
+		return clonedList;
+	}
+}
+
+void destroyList(NodeList*& list) {
+	if (list == NULL) {
+		return;
+	}
+	else {
+		while (list != NULL) {
+			NodeList* nodeToDelete = list;
+			list = list->next;
+			delete nodeToDelete;
+		}
+		delete list;
+	}
+}
+
+void displayData(NodeList* l) {
+	if (l == NULL) {
+		return;
+	}
+	else {
+		cout << l->data << " ";
+		displayData(l->next);
+	}
+}
+
+/*End of auxiliary methods */
+
+IntDictionary* createIntDictionary(unsigned int quantity) {
+	IntDictionary* id = new IntDictionary();
+	id->capacity = quantity;
+	id->elementQuantity = 0;
+	id->table = new NodeList * [quantity];
+
+	//initialize the table
+	for (int i = 0;i < quantity;i++) {
+		id->table[i] = NULL;
+	}
+	return id;
+}
+
+bool isEmpty(IntDictionary* dictionary) {
+	return dictionary->elementQuantity == 0;
+}
+
+int numberOfElements(IntDictionary* dictionary) {
+	return dictionary->elementQuantity;
+}
+
+bool belongs(IntDictionary* dictionary, int e) {
+	if (isEmpty(dictionary)) {
+		return false;
+	}
+	else {
+		int elementPositionOnTable = modHash(e, dictionary->capacity);
+		NodeList* aux = dictionary->table[elementPositionOnTable];
+		while (aux != NULL) {
+			if (aux->data == e) {
+				return true;
+			}
+			aux = aux->next;
+		}
+		return false;
+	}
+}
+
+void add(IntDictionary*& dictionary, int e) {
+	int elementPositonOnTable = modHash(e, dictionary->capacity);
+	if (!belongs(dictionary, e)) {
+		addElement(dictionary->table[elementPositonOnTable], e);
+		dictionary->elementQuantity++;
+	}
+}
+
+void erase(IntDictionary*& dictionary, int e) {
+	int elementPositionOnTable = modHash(e, dictionary->capacity);
+	if (belongs(dictionary, e)) {
+		NodeList* actualNode = dictionary->table[elementPositionOnTable];
+		NodeList* previousNode = NULL;
+
+		while (actualNode != NULL && actualNode->data != e) {
+			previousNode = actualNode;
+			actualNode = actualNode->next;
+		}
+		if (previousNode != NULL) {
+			previousNode->next = actualNode->next;
+		}
+		else {
+			dictionary->table[elementPositionOnTable] = actualNode->next;
+			delete actualNode;
+		}
+		dictionary->elementQuantity--;
+	}
+}
+
+int element(IntDictionary*& id) {
+	if (!isEmpty(id)) {
+		//return the first element found
+		for (int i = 0; i < id->capacity; i++) {
+			if (id->table[i] != NULL) {
+				return id->table[i]->data;
+			}
+		}
+	}
+}
+
+IntDictionary* clone(IntDictionary* dictionary) {
+	IntDictionary* clonedDictionary = createIntDictionary(dictionary->capacity);
+	if (isEmpty(dictionary)) {
+		return clonedDictionary;
+	}
+	else {
+		for (int i = 0; i < dictionary->capacity; i++) {
+			clonedDictionary->table[i] = cloneList(dictionary->table[i]);
+		}
+		clonedDictionary->elementQuantity = dictionary->elementQuantity;
+		return clonedDictionary;
+	}
+}
+
+void destroy(IntDictionary*& dictionary) {
+	for (int i = 0; i < dictionary->capacity; i++) {
+		destroyList(dictionary->table[i]);
+	}
+	delete dictionary->table;
+	dictionary->capacity = 0;
+	dictionary->elementQuantity = 0;
+}
+
+void display(IntDictionary*& id) {
+	if (!isEmpty(id)) {
+		for (int i = 0; i < id->capacity; i++) {
+			cout << i << "-";
+			displayData(id->table[i]);
+		}
+	}
+}
+
+
+int main() {
+
+	IntDictionary* d = createIntDictionary(10);
+	add(d, 987654329);
+	add(d, 987456123);
+	add(d, 123098564);
+	add(d, 123421456);
+	add(d, 123651456);
+	add(d, 123653456);
+
+	//erase(d, 123653456);
+
+	//IntDictionary* clonedDictionary = clone(d);
+
+	//destroy(clonedDictionary);
+
+	display(d);
+}
+
--- README.md
+++ README.md
@@ -11,6 +11,9 @@ You will find different proyects with a specific topic to practice or learn.
 
 ## Proyect Folders
 
+### Dictionary
+Contains one or multiple abstract representations of a Dictionary.
+
 ### Essentials
 Contains various examples about arrays, structures, pointers, functions and more.
 
